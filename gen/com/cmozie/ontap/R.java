21e3fdc5d447f9d3ec6cfa9b78efcd6b11ecdf2d
Cameron Mozie <camsw0rld14@gmail.com>
2013-12-01 15:12:39 -0500

    Splash Screen UI

--- gen/com/cmozie/ontap/R.java
+++ gen/com/cmozie/ontap/R.java
@@ -22,9 +22,15 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int logo=0x7f020001;
+        public static final int logotext=0x7f020002;
+        public static final int texturebkg=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int disclaimer=0x7f080001;
+        public static final int logo=0x7f080000;
+        public static final int logotext=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -36,6 +42,8 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int this_application_does_not_support_under_age_drinking_so_to_help_alleviate_minors_obtaining_information_on_alcholic_beverages_please_vefiy_your_birthdate_=0x7f050003;
+        public static final int todo=0x7f050004;
     }
     public static final class style {
         /** 
