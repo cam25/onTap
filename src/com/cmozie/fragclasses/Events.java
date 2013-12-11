97f8d6eab0eb10b69fc86a5f9675975951495fb8
Cameron Mozie <camsw0rld14@gmail.com>
2013-12-11 16:16:15 -0500

    Events not looping...

--- src/com/cmozie/fragclasses/Events.java
+++ src/com/cmozie/fragclasses/Events.java
@@ -9,12 +9,30 @@
  */
 package com.cmozie.fragclasses;
 
-import com.cmozie.ontap.R;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Fragment;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.cmozie.ontap.R;
 
 // TODO: Auto-generated Javadoc
 /**
@@ -22,6 +40,19 @@ import android.view.ViewGroup;
  */
 public class Events extends Fragment {
 
+	
+	
+	protected static final String TITLE = null;
+	public TextView eventName;
+	public TextView eventType;
+	public TextView eventDescription;
+	public TextView eventVenue;
+	public TextView eventsWebsite;
+	public TextView eventSchedule;
+	public static HashMap<String, String> myMap;
+	public static List<Map<String,String>> arrayList;
+	
+	
 	/* (non-Javadoc)
 	 * @see android.app.Fragment#onCreateView(android.view.LayoutInflater, android.view.ViewGroup, android.os.Bundle)
 	 */
@@ -33,4 +64,185 @@ public class Events extends Fragment {
 		return mainView;
 	}
 
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onActivityCreated(savedInstanceState);
+		 eventName = (TextView)getActivity().findViewById(R.id.eventName);
+		 eventType = (TextView)getActivity().findViewById(R.id.eventType);
+		 eventDescription = (TextView)getActivity().findViewById(R.id.eventDescription);
+		 eventVenue = (TextView)getActivity().findViewById(R.id.venue);
+		 eventsWebsite = (TextView)getActivity().findViewById(R.id.eventWebsite);
+		eventSchedule = (TextView)getActivity().findViewById(R.id.scheduleEvent);
+		 getApiResults();
+		
+		 
+		 eventSchedule.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				
+				  Intent intent = new Intent(Intent.ACTION_EDIT);
+				  intent.setType("vnd.android.cursor.item/event");
+				  
+				  intent.putExtra("title", eventName.getText().toString());
+				  intent.putExtra("description", eventDescription.getText().toString());
+				  intent.putExtra("availability", 1);
+				  startActivity(intent);
+
+				
+			}
+		});
+		 
+		 
+	}
+public void getApiResults(){
+		
+		String baseUrl = "http://api.brewerydb.com/v2/events?key=4b77a2665f85f929d4a87d30bbeae67b";
+		String queryString;
+		try {
+			queryString = URLEncoder.encode(baseUrl,"UTF-8");
+		} catch (Exception e) {
+			// TODO: handle exception
+			Log.e("ERROR-URL", "ENCODING ISSUE");
+			queryString = "";
+		}
+		URL finalURL;
+		try {
+			 finalURL = new URL(baseUrl);
+			 EventRequest evntRequest = new EventRequest();
+			 evntRequest.execute(finalURL);
+		} catch (Exception e) {
+			// TODO: handle exception
+			Log.i("BAD URL", "URL MALFORMED");
+		}
+		
+		
+	}
+	
+	
+	private class EventRequest extends AsyncTask<URL, Void, String>{
+	
+		@Override
+		protected void onPreExecute() {
+			// TODO Auto-generated method stub
+			super.onPreExecute();
+			
+		}
+
+		@Override
+		protected String doInBackground(URL... urls) {
+			// TODO Auto-generated method stub
+			String reply = "";
+			for (URL url : urls) {
+			reply = Network.URLStringResponse(url);	
+			}
+			return reply;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			// TODO Auto-generated method stub
+			arrayList = new ArrayList<Map<String,String>>();
+			try {
+				JSONObject json = new JSONObject(result);
+				JSONArray data = json.getJSONArray("data");
+				
+				
+				
+				
+				for (int i = 0; i < data.length(); i++) {
+					
+					
+					
+					
+					JSONObject allEvents = data.getJSONObject(i);
+					myMap = new HashMap<String, String>();
+					Log.i("events", allEvents.toString());
+					//Log.i("events", allEvents.toString());
+					if (allEvents.has("name")) {
+						String name = allEvents.getString("name");
+						myMap.put("name", name);
+						Log.i("map", myMap.toString());
+						//eventName.setText(name);
+					}
+						
+					if (allEvents.has("description")) {
+						String descript = allEvents.getString("description");
+						myMap.put("description", descript);
+						//eventDescription.setText(descript);
+					}
+					if (allEvents.has("type")) {
+						String typeEvent = allEvents.getString("type");
+						myMap.put("type", typeEvent);
+						//eventType.setText(typeEvent);
+					}
+					if (allEvents.has("venueName")) {
+						String venueEvent = allEvents.getString("venueName");
+						myMap.put("venueName", venueEvent);
+						//eventVenue.setText(venueEvent);
+					}
+					
+					if (allEvents.has("website")) {
+						String eventWebsite = allEvents.getString("website");
+						myMap.put("website", eventWebsite);
+						//eventsWebsite.setText(eventWebsite);
+					}
+					
+					arrayList.add(myMap);
+					
+					for (int j = 0; j <  arrayList.size(); j++) {
+						
+						if (allEvents.has("name")) {
+							String name = allEvents.getString("name");
+							eventName.setText(name);
+						}
+							
+						if (allEvents.has("description")) {
+							String descript = allEvents.getString("description");
+							eventDescription.setText(descript);
+						}
+						if (allEvents.has("type")) {
+							String typeEvent = allEvents.getString("type");
+							eventType.setText(typeEvent);
+						}
+						if (allEvents.has("venueName")) {
+							String venueEvent = allEvents.getString("venueName");
+							eventVenue.setText(venueEvent);
+						}
+						
+						if (allEvents.has("website")) {
+							String eventWebsite = allEvents.getString("website");
+							eventsWebsite.setText(eventWebsite);
+						}
+					}
+				}
+				
+				
+				
+				
+				
+			
+				
+				
+				
+				
+				//beerDescription.setText(description2);
+				//Log.i("beer", beerNam);
+			} catch (JSONException e) {
+				// TODO: handle exception
+				
+				e.printStackTrace();
+				//beerName.setText(beerNam);
+				//beerDescription.setText("No description");
+				
+			}
+			
+		}
+			
+	}
 }
+		
+	
